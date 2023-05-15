dd6fa7ac42ce20b7354ab3403cba1736fc9f9139
sigh-gone <a1@Als-MacBook-Pro.local>
2023-05-15 15:43:18 -0500

    search hex

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use std::{fs, io};
+use std::{
+    fs::{self, File},
+    io::{self, Read},
+};
+
+use hex::ToHex;
 
 #[cfg(test)]
 mod tests {
@@ -20,6 +25,38 @@ mod tests {
         }
         assert!(false);
     }
+
+    #[test]
+    pub fn new_try2() {
+        let data = &[
+            0x6A, 0x40, 0x68, 0x00, 0x30, 0x00, 0x00, 0x6A, 0x14, 0x8D, 0x91,
+        ];
+        let pattern = &[
+            0x8D, 0x4D, 0xB0, 0x2B, 0xC1, 0x83, 0xC0, 0x27, 0x99, 0x6A, 0x4E, 0x59, 0xF7, 0xF9,
+        ];
+
+        if let Some(position) = search_hex_bytes(data, pattern) {
+            println!("Pattern found at position: {}", position);
+        } else {
+            println!("Pattern not found");
+        }
+    }
+
+    #[test]
+    pub fn new_try3() {
+        let hex_string = "6865".to_owned();
+        let pattern = hex::decode(hex_string).unwrap();
+        let smoke = read_file_to_hex("./hello.txt").unwrap();
+        println!("{:?}", smoke);
+        let data = hex::decode(smoke).unwrap();
+
+        if let Some(offset) = search_pattern(&data, &pattern) {
+            println!("Pattern found at offset: {}", offset);
+        } else {
+            println!("Pattern not found");
+        }
+        assert!(false);
+    }
 }
 fn search_pattern_in_file(file_path: &str, pattern: &[u8]) -> io::Result<Option<usize>> {
     let bytes = fs::read(file_path)?;
@@ -33,4 +70,34 @@ fn search_pattern_in_file(file_path: &str, pattern: &[u8]) -> io::Result<Option<
     }
 
     Ok(None)
+}
+
+fn search_hex_bytes(data: &[u8], pattern: &[u8]) -> Option<usize> {
+    data.windows(pattern.len())
+        .position(|window| window == pattern)
+}
+
+fn read_file_to_hex(filename: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(filename)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+
+    Ok(buffer.encode_hex::<String>())
+}
+
+fn search_pattern(data: &[u8], pattern: &[u8]) -> Option<usize> {
+    let pattern_len = pattern.len();
+    let data_len = data.len();
+
+    if pattern_len > data_len {
+        return None;
+    }
+
+    for i in 0..=data_len - pattern_len {
+        if data[i..i + pattern_len] == *pattern {
+            return Some(i);
+        }
+    }
+
+    None
 }
