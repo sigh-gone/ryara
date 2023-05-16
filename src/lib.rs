1a779187399e2458cbf7b5b2532b12c0da629732
sigh-gone <a1@Als-MacBook-Pro.local>
2023-05-16 08:22:06 -0500

    cleanup and refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,21 @@
-use std::{
-    fs::{self, File},
-    io::{self, Read},
-};
+use std::{fs::File, io::Read};
 
 use hex::ToHex;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::File;
-    use std::io::BufReader;
-    use std::io::{Bytes, Read};
-    use std::time::{Duration, Instant};
 
     #[test]
-    pub fn new_try() {
-        let file_path = "./hello.txt";
-        let pattern: &[u8] = &[104, 101, 108];
-
-        match search_pattern_in_file(file_path, pattern) {
-            Ok(Some(position)) => println!("Pattern found at position: {}", position),
-            Ok(None) => println!("Pattern not found."),
-            Err(error) => eprintln!("Error: {}", error),
-        }
-        assert!(false);
-    }
-
-    #[test]
-    pub fn new_try2() {
-        let data = &[
-            0x6A, 0x40, 0x68, 0x00, 0x30, 0x00, 0x00, 0x6A, 0x14, 0x8D, 0x91,
-        ];
-        let pattern = &[
-            0x8D, 0x4D, 0xB0, 0x2B, 0xC1, 0x83, 0xC0, 0x27, 0x99, 0x6A, 0x4E, 0x59, 0xF7, 0xF9,
-        ];
-
-        if let Some(position) = search_hex_bytes(data, pattern) {
-            println!("Pattern found at position: {}", position);
-        } else {
-            println!("Pattern not found");
-        }
-    }
-
-    #[test]
-    pub fn new_try3() {
-        let hex_string = "6865".to_owned();
-        let pattern = hex::decode(hex_string).unwrap();
+    pub fn comparing_hex_files() {
         let smoke = read_file_to_hex("./hello.txt").unwrap();
-        println!("{:?}", smoke);
+        let smoke2 = read_file_to_hex("./hello2.txt").unwrap();
+
         let data = hex::decode(smoke).unwrap();
+        let pattern = hex::decode(smoke2).unwrap();
 
-        if let Some(offset) = search_pattern(&data, &pattern) {
-            println!("Pattern found at offset: {}", offset);
-        } else {
-            println!("Pattern not found");
-        }
-        assert!(false);
+        assert!(search_pattern(&data, &pattern).is_some());
     }
-}
-fn search_pattern_in_file(file_path: &str, pattern: &[u8]) -> io::Result<Option<usize>> {
-    let bytes = fs::read(file_path)?;
-    let pattern_len = pattern.len();
-    let bytes_len = bytes.len();
-
-    for i in 0..=(bytes_len - pattern_len) {
-        if bytes[i..i + pattern_len] == *pattern {
-            return Ok(Some(i));
-        }
-    }
-
-    Ok(None)
-}
-
-fn search_hex_bytes(data: &[u8], pattern: &[u8]) -> Option<usize> {
-    data.windows(pattern.len())
-        .position(|window| window == pattern)
 }
 
 fn read_file_to_hex(filename: &str) -> Result<String, std::io::Error> {
